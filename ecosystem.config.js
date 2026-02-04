9e2b5368a82b37e71f1fafc68a5542de8494fcb2
FredsonPOSantos <son.magometal@gmail.com>
2026-02-04 16:56:41 -0300

    correção acesso sistema ejs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         PORT: 3001,
         // [CORRIGIDO] IP do servidor onde o painel de ADMINISTRAÇÃO (que serve as imagens) está a ser executado.
         // Não use '127.0.0.1' ou 'localhost' em produção.
-        ADM_SERVER_IP: "10.0.0.45"
+        ADM_SERVER_IP: "10.0.0.45",
       }
     }
   ]
